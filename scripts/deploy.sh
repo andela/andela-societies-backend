db2742dd2dfbc3612dac20b15dba20c7dbb0eaa6
WinstonKamau <crispus.kamau@andela.com>
2018-05-23 13:26:58 +0300

    feat(CD): create continuous deployment pipeline
    - Create continuous deployment pipeline.
    - Each deployment to staging will change the deployment image.
    - The deployment image is the image that the backend container is running
    
    [ Fixes #157804399 ]

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,19 @@
 set -o errexit
 set -o pipefail
 
-tag_branch() {
+set_variables() {
     COMMIT_HASH=$(git rev-parse --short HEAD)
 
     if [ "$CIRCLE_BRANCH" == 'master' ]; then
         IMAGE_TAG=$COMMIT_HASH
+        ENVIRONMENT=production
+        GOOGLE_COMPUTE_ZONE=${PRODUCTION_ZONE}
+        GOOGLE_CLUSTER_NAME=${PRODUCTION_CLUSTER_NAME}
     else
         IMAGE_TAG="${CIRCLE_BRANCH}-${COMMIT_HASH}"
+        ENVIRONMENT=staging
+        GOOGLE_COMPUTE_ZONE=${STAGING_ZONE}
+        GOOGLE_CLUSTER_NAME=${STAGING_CLUSTER_NAME}
     fi
 }
 
@@ -30,10 +36,46 @@ deploy_image() {
     make publish
 }
 
+install_google_cloud_sdk(){
+    echo "====> Installing google cloud sdk"
+    echo "deb http://packages.cloud.google.com/apt cloud-sdk-jessie main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+    sudo apt-get update && sudo apt-get install kubectl google-cloud-sdk
+}
+
+configure_google_cloud_sdk() {
+    echo "Configuring Google Cloud Sdk"
+    gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
+    gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
+    gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
+    gcloud --quiet container clusters get-credentials ${GOOGLE_CLUSTER_NAME}
+}
+
+deploy_to_kubernetes(){
+     echo "====> Prepare image for deployement"
+
+    IMAGE="${DOCKER_REGISTRY}/${GOOGLE_PROJECT_ID}/${PROJECT_NAME}:${IMAGE_TAG}"
+    DEPLOYMENT_NAME="${ENVIRONMENT}-${PROJECT_NAME}"
+    echo "====> Deploying ${IMAGE} to ${DEPLOYMENT_NAME} in ${ENVIRONMENT} environment"
+
+
+    kubectl set image deployment/${DEPLOYMENT_NAME} backend=${IMAGE} -n "${ENVIRONMENT}"
+
+    if [ "$?" == "0" ]; then
+        echo "Deployment completed succesfully"
+    else
+        echo "Failed to deploy ${IMAGE} to ${ENVIRONMENT} environment"
+    fi
+
+}
+
 main() {
-    tag_branch
+    set_variables
     authorize_docker
     deploy_image
+    install_google_cloud_sdk
+    configure_google_cloud_sdk
+    deploy_to_kubernetes
 }
 
 main "$@"
