a52d4777e683d6d5758bd3c97f7bd6ebeed934ad
strongestavenger <mikemutoro@gmail.com>
2019-01-25 10:30:21 +0300

    configure slack notification helper

---
+++ src/api/services/slack-notify/notification.py
@@ -0,0 +1,44 @@
+import os
+import logging
+
+from slackclient import SlackClient
+
+
+class SlackNotification(object):
+
+    def __init__(self):
+        slack_token = os.getenv('SLACK_API_TOKEN')
+        if slack_token:
+            self.sc = SlackClient(slack_token)
+
+
+
+    def get_slack_id(self, user):
+        user_email = user.email
+        results = sc.api_call("users.list")
+        users = results.get("members")
+
+        if users:
+            user_id = [
+                user.get("id")
+                for user in users
+                if user.get("profile").get("email") == user_email
+            ]
+
+        try:
+            return user_id[0]
+        except Exception:
+            logging.info("User not found")
+            return None
+
+
+    def send_message(self, message, slack_id):
+        sc.api_call(
+            "chat.postEphemeral",
+            channel=slack_id,
+            text=message,
+            username='@notifier',
+            user=slack_id,
+            as_user=True,
+            icon_emoji=':ninja:',
+        )
