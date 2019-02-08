285df2c0be9118b485261a6efce8ccaac33571d3
strongestavenger <mikemutoro@gmail.com>
2019-02-08 11:44:52 +0300

    add success ops notification

--- src/api/endpoints/logged_activities/secretary_review.py
+++ src/api/endpoints/logged_activities/secretary_review.py
@@ -3,11 +3,13 @@ from flask import request, g
 
 from api.services.auth import token_required, roles_required
 from api.utils.helpers import response_builder
+from api.models import Role, User
+from api.services.slack_notify import SlackNotification
 
 from .marshmallow_schemas import single_logged_activity_schema
 
 
-class SecretaryReviewLoggedActivityAPI(Resource):
+class SecretaryReviewLoggedActivityAPI(Resource, SlackNotification):
     """Enable society secretary to verify logged activities."""
 
     decorators = [token_required]
@@ -15,6 +17,7 @@ class SecretaryReviewLoggedActivityAPI(Resource):
     def __init__(self, **kwargs):
         """Inject dependency for resource."""
         self.LoggedActivity = kwargs['LoggedActivity']
+        SlackNotification.__init__(self)
 
     @roles_required(['society secretary'])
     def put(self, logged_activity_id):
@@ -41,7 +44,21 @@ class SecretaryReviewLoggedActivityAPI(Resource):
         if not (payload.get('status') in ['pending', 'rejected']):
             return response_builder(dict(message='Invalid status value.'),
                                     400)
+
+        roles = User.query.filter(User.roles.any(Role.name=="success ops")).all()
+        users = User.query.all()
+        message = "New society points approved. Go to https://societies.andela.com to give redemption approval"
+
         logged_activity.status = payload.get('status')
+        if logged_activity.status == "pending":
+            for role in roles:
+                if role in users:
+                    user_email = role.email
+                    print("the success ops member is: ", user_email)
+                    slack_id = SlackNotification.get_slack_id(self, user_email)
+                    SlackNotification.send_message(self, message, slack_id)
+
+        print("the logged status is: ", logged_activity.status)
         logged_activity.save()
 
         return response_builder(
