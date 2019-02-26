3ab9c2a43fb705eb8caa8acf27706b722dda7c16
strongestavenger <mikemutoro@gmail.com>
2019-02-26 11:53:56 +0300

    add send notification functionality

--- src/api/endpoints/redemption_requests/redemption_numeration.py
+++ src/api/endpoints/redemption_requests/redemption_numeration.py
@@ -93,11 +93,11 @@ class RedemptionRequestNumeration(Resource, SlackNotification):
                 recipients=[finance_email]
             )
 
-            self.email.send(
-                current_app._get_current_object(),
-                payload=email_payload,
-                mail=self.mail
-            )
+            # self.email.send(
+            #     current_app._get_current_object(),
+            #     payload=email_payload,
+            #     mail=self.mail
+            # )
 
             email_payload = dict(
                 sender=current_app.config["SENDER_CREDS"],
@@ -108,11 +108,21 @@ class RedemptionRequestNumeration(Resource, SlackNotification):
                 recipients=[redemp_request.user.email]
             )
 
-            self.email.send(
-                current_app._get_current_object(),
-                payload=email_payload,
-                mail=self.mail
-            )
+            # Send Slack notification to Society President
+            message = f"Redemption Request on" + \
+                      f" *{redemp_request.name}*" + \
+                      f" has been approved." + \
+                      f" Finance will be in touch"
+            user_email = redemp_request.user.email
+            print(user.email)
+            slack_id = SlackNotification.get_slack_id(self, user_email)
+            SlackNotification.send_message(self, message, slack_id)
+
+            # self.email.send(
+            #     current_app._get_current_object(),
+            #     payload=email_payload,
+            #     mail=self.mail
+            # )
         elif status == "rejected":
             redemp_request.status = status
             redemp_request.rejection = rejection_reason
