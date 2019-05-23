f956556a9fc961a0ecd0910cdf9f0d76eef03bd3
strongestavenger <mikemutoro@gmail.com>
2019-05-23 14:28:49 +0300

    add branch to circle-ci build
    configure new branch
    
    deploy current branch
    
    deploy current branch
    
    reconfigure build and deply scripts
    
    remove commented out code
    
    ft remove unused imports
    
    finalize
    
    fix failing tests
    
    fix failing tests
    
    squash commits
    
    make variable global
    
    make variable global
    
    squash commits

--- src/manage.py
+++ src/manage.py
@@ -146,6 +146,3 @@ migrate = Migrate(app, db)
 
 if __name__ == "__main__":
     cli()
-    # gunicorn_logger = logging.getLogger('gunicorn.error')
-    # app.logger.handlers = gunicorn_logger.handlers
-    # app.logger.setLevel(gunicorn_logger.level)
