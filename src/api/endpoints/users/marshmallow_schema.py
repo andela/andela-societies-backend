e82c643633dc43a312f38f53bd5e730e23cced51
strongestavenger <mikemutoro@gmail.com>
2019-08-09 12:43:51 +0300

    refactor

--- src/api/endpoints/users/marshmallow_schema.py
+++ src/api/endpoints/users/marshmallow_schema.py
@@ -13,16 +13,6 @@ class UserSchema(BaseSchema):
     cohort_id = fields.String(dump_only=True, dump_to='cohortId',
                               validate=[validate.Length(max=36)])
 
-class UsersSchema(BaseSchema):
-    """Users serializer/validator."""
 
-    society_id = fields.String(dump_only=True, dump_to='societyId',
-                               validate=[validate.Length(max=36)])
-    center_id = fields.String(dump_only=True, dump_to='centerId',
-                              validate=[validate.Length(max=36)])
-    cohort_id = fields.String(dump_only=True, dump_to='cohortId',
-                              validate=[validate.Length(max=36)])
-
-
-users_schema = UsersSchema(many=True)
+users_schema = UserSchema(many=True)
 user_schema = UserSchema()
